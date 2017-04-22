96c7a53af9b207cfa87ea231f3abc3c4ce5753c9
masaaki1915 <masaaki1915@gmail.com>
2017-04-22 12:33:42 +0900

    email検証

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence:   true, length: { maximum: 255 },
-                    format:     { with: VALID_EMAIL_REGEX },
+  before_save :email_downcase
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  private
+
+    def email_downcase
+      email.downcase!
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -35,14 +35,21 @@ class UserTest < ActiveSupport::TestCase
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
-      assert_not @user.valid?, "#{valid_address.inspect} should be valid"
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user = @user.email.upcase
+    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
